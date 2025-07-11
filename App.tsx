310afdf74caa501ff550bd3ebcdcea2d7e7c9358
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-11 19:53:23 +0200

    Added colors and gradient

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import { SafeAreaView, StyleSheet } from 'react-native';
 import HabitScreen from './components/habits/HabitScreen';
+import { LinearGradient } from 'expo-linear-gradient';
+import Colors from './lib/colors/Colors';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <HabitScreen />
-    </SafeAreaView>
+    <LinearGradient colors={[Colors.primary, Colors.secondary]} style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <HabitScreen />
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 32,
+    paddingTop: 24,
+    flex: 1,
     paddingHorizontal: 8,
     width: '100%',
     height: '100%',
---
+++ lib/colors/Colors.ts
@@ -0,0 +1,8 @@
+const Colors = {
+  primary: '#5fb4ffff',
+  secondary: '#ffffffff',
+  background: '#ffffff',
+  text: '#333333',
+};
+
+export default Colors;
