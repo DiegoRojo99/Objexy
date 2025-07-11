437dba7dfa8d4048fe1a41ae3302c4bd795dbbee
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-11 15:54:32 +0200

    Added streak

--- components/habits/HabitItem.tsx
+++ components/habits/HabitItem.tsx
@@ -1,8 +1,9 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
-import { Habit } from "../../lib/types/Habit";
+import { View, Text, StyleSheet, Pressable, Image, Button } from "react-native";
+import { HabitWithLogs } from "../../lib/types/Habit";
 import TagList from "../tags/TagList";
+import HabitLogStreak from "./HabitLogStreak";
 
-export default function HabitItem({ habit }: { habit: Habit }) {
+export default function HabitItem({ habit }: { habit: HabitWithLogs }) {
   function formatFrequency(period: string, count: number): string {
     const uppercasePeriod = period.charAt(0).toUpperCase() + period.slice(1);
     return `Frequency: ${uppercasePeriod} x${count}`;
@@ -10,35 +11,51 @@ export default function HabitItem({ habit }: { habit: Habit }) {
 
   return (
     <Pressable style={styles.itemContainer} android_ripple={{ color: '#ddd' }}>
+      <View style={styles.topRow}>
+        <Image
+          source={habit.icon ? { uri: habit.icon } : require('../../assets/images/goal.png')}
+          style={{ flex: 0, width: 40, height: 40, borderRadius: 20, marginRight: 8 }}
+        />
+        <View style={{ flex: 4 }}>
+          <Text style={styles.itemName}>{habit.name}</Text>
+          <Text style={styles.itemDescription}>{habit.description}</Text>
+        </View>
+        <View style={{ flex: 0, width: 40, height: 40, alignItems: 'center' }}>
+          <Button title="+" onPress={() => {}} />
+        </View>
+      </View>
       <View>
-        <Text style={styles.itemName}>{habit.name}</Text>
-        <Text style={styles.itemDescription}>{habit.description}</Text>
         <Text style={styles.itemFrequency}>{formatFrequency(habit.targetPeriod, habit.targetCount)}</Text>
         <TagList tags={habit.tagIds} />
       </View>
+      <HabitLogStreak logs={habit.logs} />
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   itemContainer: {
+    width: '100%',
     padding: 8,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 4,
     marginVertical: 8,
   },
+  topRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
   itemName: {
     fontWeight: 'bold',
-    fontSize: 18,
-    marginBottom: 8,
-    paddingBottom: 4,
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-    textAlign: 'center',
+    fontSize: 12,
+    textAlign: 'left',
   },
   itemDescription: {
-    marginVertical: 5,
+    fontSize: 12,
+    color: '#666',
+    textAlign: 'left',
   },
   itemFrequency: {
     marginVertical: 5,
---
+++ components/habits/HabitLogStreak.tsx
@@ -0,0 +1,75 @@
+import { Text, View, StyleSheet } from "react-native";
+import { HabitLog } from "../../lib/types/Habit";
+
+export default function HabitLogStreak({ logs }: { logs: HabitLog[] }) {
+  function formatDate(date: string): string {
+    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Date(date).toLocaleDateString('en-US', options);
+  }
+
+  function checkPreviousDay(date1: string, date2: string): boolean {
+    const currDate = new Date(date1);
+    const prevDate = new Date(date2);
+
+    if (isNaN(prevDate.getTime()) || isNaN(currDate.getTime())) return false;
+    // Set the time to midnight for both dates
+    prevDate.setHours(0, 0, 0, 0);
+    currDate.setHours(0, 0, 0, 0);
+
+    console.log(`Checking if ${formatDate(date1)} is the previous day of ${formatDate(date2)}`);
+    console.log(`Previous Date: ${prevDate.getTime()}, Current Date: ${currDate.getTime()}`);
+    console.log(`Diff: ${currDate.getTime() - prevDate.getTime()}`);
+    console.log(`Result: ${prevDate.getTime() === currDate.getTime() - 86400000}`);
+
+
+    // Check if the previous date is exactly one day before the current date
+    return prevDate.getTime() === currDate.getTime() - 86400000;
+  }
+
+  const calculateStreak = (logs: HabitLog[]): number => {
+    // If no logs, return 0
+    if (logs.length === 0) return 0;
+
+    // Sort logs by date in descending order
+    logs.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    let streak = 0;
+    for (let i = 0; i < logs.length; i++) {
+      // Check if the current log is the first one
+      if (i === 0) streak++;
+      // Check if the current log is consecutive with the previous one
+      else if (checkPreviousDay(logs[i - 1].date, logs[i].date)) {
+        streak++;
+      }
+      // If not consecutive, break the loop
+      else {
+        break;
+      }
+    }
+    return streak;
+  };
+
+  const streak = calculateStreak(logs);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Current Streak</Text>
+      <Text style={styles.streakCount}>{streak} days</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    borderRadius: 8,
+    backgroundColor: '#f9f9f9',
+    marginVertical: 8,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  streakCount: {
+    fontSize: 14,
+    color: '#666',
+  },
+});
