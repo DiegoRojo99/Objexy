6c16b000c0bcf7f1e5222c240c2634398e1dee85
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-12 17:32:28 +0200

    Added gradient background

---
+++ components/bg/GradientBackground.tsx
@@ -0,0 +1,21 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet } from 'react-native';
+import React from 'react';
+
+export const GradientBackground: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
+  <LinearGradient
+    colors={['#0C3026', '#155854']}
+    style={styles.gradient}
+  >
+    {children}
+  </LinearGradient>
+);
+
+const styles = StyleSheet.create({
+  gradient: {
+    paddingTop: 32,
+    paddingHorizontal: 8,
+    flex: 1,
+    color: 'white'
+  },
+});
