7a223ba53eed3e96dafd549d6e9c1d938f0e99d9
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-12 16:47:12 +0200

    Added stylesheet

--- components/habits/HabitList.tsx
+++ components/habits/HabitList.tsx
@@ -1,14 +1,24 @@
 import { FlatList } from "react-native";
 import { HabitWithLogs } from "../../lib/types/Habit";
 import HabitItem from "./HabitItem";
+import { StyleSheet } from "react-native";
 
 export function HabitList({ habits }: { habits: HabitWithLogs[] }) {
   return (
     <FlatList
-      style={{ paddingTop: 8, marginBottom: 24, flex: 1 }}
+      style={styles.habitListContainer}
       data={habits}
       renderItem={({ item }) => <HabitItem key={item.id} habit={item} />}
       keyExtractor={item => item.id}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  habitListContainer: {
+    flex: 1,
+    paddingHorizontal: 4,
+    paddingTop: 8,
+    marginVertical: 8,
+  },
+});
