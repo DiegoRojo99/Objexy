ac8b5fdfb3f9e1aaa29a69b8158ab845894ee16d
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-12 16:46:39 +0200

    Added bottom tabs

--- App.tsx
+++ App.tsx
@@ -3,9 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HabitsScreen from './screens/HabitsScreen';
 import HabitDetailsScreen from './screens/HabitDetailsScreen';
-import Colors from './lib/colors/Colors';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
 
 export default function App() {
   useFonts({
@@ -13,27 +17,75 @@ export default function App() {
     'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
   });
 
-  const commonScreenOptions = {
-    headerTitleStyle: { fontFamily: 'Roboto-Bold' },
-    headerStyle: { backgroundColor: Colors.navigation },
-    headerTintColor: Colors.navigationText,
+  const TabScreenOptions = {
+    headerShown: false,
+    tabBarActiveTintColor: '#6200ee',
+    tabBarInactiveTintColor: '#999',
+    tabBarStyle: {
+      backgroundColor: '#fff',
+      borderTopWidth: 1,
+      elevation: 0,
+      marginBottom: 0,
+    },
   };
 
+  const commonScreenOptions = {
+    headerShown: false,
+    contentStyle: {
+      paddingTop: 32,
+      paddingHorizontal: 8,
+      flex: 1,
+    },
+  };
+
+  function HomeScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    )
+  }
+
+  function HabitsStack() {
+    return (
+      <Stack.Navigator screenOptions={commonScreenOptions}>
+        <Stack.Screen 
+          name="HabitsList" 
+          component={HabitsScreen} 
+        />
+        <Stack.Screen 
+          name="HabitDetails" 
+          component={HabitDetailsScreen}
+        />
+      </Stack.Navigator>
+    );
+  }
+
   return (
+    <>
+      <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator screenOptions={commonScreenOptions}>
-          <Stack.Screen 
-            name="Habits" 
-            component={HabitsScreen} 
+        <BottomTabs.Navigator screenOptions={TabScreenOptions}>
+          <BottomTabs.Screen 
+            name="Home" 
+            component={HomeScreen} 
             options={{ 
-              headerTitle: 'My Habits'
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" size={size} color={color} />
+              ),
             }}
           />
-          <Stack.Screen 
-            name="Habit Details" 
-            component={HabitDetailsScreen}
+          <BottomTabs.Screen 
+            name="HabitsTab" 
+            component={HabitsStack} 
+            options={{ 
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="list" size={size} color={color} />
+              ),
+            }}
           />
-        </Stack.Navigator>
+        </BottomTabs.Navigator>
       </NavigationContainer>
+    </>
   );
 }
